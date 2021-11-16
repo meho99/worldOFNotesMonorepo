83f9b769b6e30d37ad873d76b00aa10b77f12930
Michal Topa <michal.topa@rumblefish.pl>
2021-11-16 14:55:51 +0100

    add input validation in signUp

--- packages/server/src/lambdas/signUp.ts
+++ packages/server/src/lambdas/signUp.ts
@@ -1,17 +1,19 @@
 require('@babel/polyfill')
 
 import faunadb from 'faunadb'
-import middy from 'middy'
+import { JSONSchemaType } from 'ajv'
 import { APIGatewayEvent, Context } from 'aws-lambda'
+import middy from 'middy'
+import validator from '@middy/validator'
 import jsonBodyParser from '@middy/http-json-body-parser'
 
-import { SignUpRequest, UserModel } from '@won/core'
-import { SingUpResponse } from '@won/core/src'
+import { SignUpRequest, UserModel, SingUpResponse } from '@won/core'
 
-import { FaunaQuery } from '../types'
+import { FaunaQuery, RequestData } from '../types'
 import { getFaunaDBClient } from '../helpers/fauna'
 import { createToken } from '../helpers/authentication'
-import { createErrorResponse, createInternalErrorResponse, createSuccessResponse } from '../helpers/responses'
+import { createErrorResponse, createSuccessResponse } from '../helpers/responses'
+import { errorsMiddleware } from '../middlewares/errorsMiddleware'
 
 const {
   Get,
@@ -23,61 +25,69 @@ const {
 
 const signUphandler = async (event: APIGatewayEvent, context: Context) => {
   const { body, httpMethod } = event
+  
+  if (httpMethod === 'POST') {
+    // at this point body will have a proper type due to validation in middleware
+    const { email, password, name } = body as unknown as SignUpRequest
 
-  try {
-    if (httpMethod === 'POST') {
-      // at this point body will have a proper type due to validation in middleware
-      const { email, password, name } = body as unknown as SignUpRequest
+    const faunaDBClient = getFaunaDBClient();
 
-      if (!email || !password || !name) {
-        return createErrorResponse({
-          message: 'please enter all fields'
-        })
-      }
-
-      const faunaDBClient = getFaunaDBClient();
-
-      try {
-        const user = await faunaDBClient.query<{ secret: string }>(
-          Get(
-            Match(
-              Index("user_by_email"),
-              email
-            )
+    try {
+      const user = await faunaDBClient.query<{ user: UserModel }>(
+        Get(
+          Match(
+            Index("user_by_email"),
+            email
           )
-        )
-
-        if (user) {
-          return createErrorResponse({
-            message: 'User already exists'
-          })
-        }
-      } catch (e) { }
-
-      const { data, ref } = await faunaDBClient.query<FaunaQuery<UserModel>>(
-        Create(
-          Collection('Users'), {
-          credentials: { password },
-          data: { email, name },
-        }
         )
       )
 
-      const jwtToken = createToken(ref.id)
-
-      const response: SingUpResponse = {
-        token: jwtToken,
-        id: ref.id,
-        name: data.name,
-        email: data.email
+      if (user) {
+        return createErrorResponse({
+          message: 'User already exists'
+        })
       }
+    } catch (e) { }
 
-      return createSuccessResponse(response)
+    const { data, ref } = await faunaDBClient.query<FaunaQuery<UserModel>>(
+      Create(
+        Collection('Users'), {
+        credentials: { password },
+        data: { email, name },
+      }
+      )
+    )
+
+    const jwtToken = createToken(ref.id)
+
+    const response: SingUpResponse = {
+      token: jwtToken,
+      id: ref.id,
+      name: data.name,
+      email: data.email
     }
-  } catch (e) {
-    return createInternalErrorResponse(e)
+
+    return createSuccessResponse(response)
+  }
+}
+
+const inputSchema: JSONSchemaType<RequestData<SignUpRequest>> = {
+  type: 'object',
+  required: ['body'],
+  properties: {
+    body: {
+      type: 'object',
+      properties: {
+        email: { type: 'string', format: 'email' },
+        password: { type: 'string' },
+        name: { type: 'string' }
+      },
+      required: ['email', 'password', 'name']
+    }
   }
 }
 
 export const handler = middy(signUphandler)
   .use(jsonBodyParser())
+  .use(validator({ inputSchema }))
+  .use(errorsMiddleware())
---
+++ packages/server/test/lambdas/signUp.test.ts
@@ -0,0 +1,162 @@
+import faunadb from 'faunadb'
+import jwt from 'jsonwebtoken'
+import lambdaTester from 'lambda-tester'
+import { APIGatewayProxyEvent } from 'aws-lambda'
+import { SignUpRequest, LoginResponse, UserModel } from '@won/core'
+
+import { FaunaQuery, JwtContent } from '../../src/types'
+import { handler } from "../../src/lambdas/signUp"
+import * as helpers from '../../src/helpers/fauna'
+import { LambdaResponse } from '../../src/helpers/responses'
+
+import { setupTestDatabase } from '../utils/fauna'
+import { createRequest } from '../utils/helpers'
+
+jest.mock('../../src/helpers/fauna', () => {
+  const helpers = jest.requireActual('../../src/helpers/fauna')
+
+  return {
+    ...helpers,
+    getFaunaDBClient: jest.fn()
+  }
+})
+
+let dbClient: faunadb.Client
+
+beforeEach(async () => {
+  const client = await setupTestDatabase();
+  if (client) dbClient = client;
+
+  (helpers.getFaunaDBClient as jest.Mock).mockImplementationOnce(() => dbClient)
+})
+
+afterAll(() => {
+  jest.clearAllMocks
+})
+
+describe("signUp", () => {
+  describe("should fail", () => {
+    it("when email is missing", async () => {
+      const requestData: SignUpRequest = {
+        password: '321536dfh'
+      } as SignUpRequest
+      const request = createRequest(requestData)
+
+      await lambdaTester(handler)
+        .event(request as APIGatewayProxyEvent)
+        .expectResult((result: LambdaResponse) => {
+          const responseBody = JSON.parse(result.body)
+
+          expect(result.statusCode).toBe(400)
+          expect(responseBody.message).toBe("Event object failed validation")
+          expect(responseBody.details[0].message).toBe("must have required property email")
+        })
+    })
+
+    it("when email is invalid", async () => {
+      const requestData: SignUpRequest = {
+        name: 'Test Name',
+        password: '321536dfh',
+        email: 'testWrongFormat'
+      }
+      const request = createRequest(requestData)
+
+      await lambdaTester(handler)
+        .event(request as APIGatewayProxyEvent)
+        .expectResult((result: LambdaResponse) => {
+          const responseBody = JSON.parse(result.body)
+
+          expect(result.statusCode).toBe(400)
+          expect(responseBody.message).toBe("Event object failed validation")
+          expect(responseBody.details[0].message).toBe(`must match format "email"`)
+        })
+    })
+
+    it("when password is missing", async () => {
+      const requestData: SignUpRequest = {
+        email: 'w@w.w',
+        name: 'Test Name'
+      } as SignUpRequest
+
+      const request = createRequest(requestData)
+
+      await lambdaTester(handler)
+        .event(request as APIGatewayProxyEvent)
+        .expectResult((result: LambdaResponse) => {
+          const responseBody = JSON.parse(result.body)
+
+          expect(result.statusCode).toBe(400)
+          expect(responseBody.message).toBe("Event object failed validation")
+          expect(responseBody.details[0].message).toBe("must have required property password")
+        })
+    })
+
+    it("when name is missing", async () => {
+      const requestData: SignUpRequest = {
+        email: 'w@w.w',
+        password: '123123123123'
+      } as SignUpRequest
+
+      const request = createRequest(requestData)
+
+      await lambdaTester(handler)
+        .event(request as APIGatewayProxyEvent)
+        .expectResult((result: LambdaResponse) => {
+          const responseBody = JSON.parse(result.body)
+
+          expect(result.statusCode).toBe(400)
+          expect(responseBody.message).toBe("Event object failed validation")
+          expect(responseBody.details[0].message).toBe("must have required property name")
+        })
+    })
+  })
+
+  describe("should suceed", () => {
+    it("with valid credentials", async () => {
+      const {
+        Get,
+        Match,
+        Index
+      } = faunadb.query
+
+      const password = 'testPassword123'
+      const email = 'test@email.com'
+      const name = 'Test User'
+
+      // -- login --
+
+      const requestData: SignUpRequest = { email, password, name }
+      const request = createRequest(requestData)
+
+      await lambdaTester(handler)
+        .event(request as APIGatewayProxyEvent)
+        .expectResult((result: LambdaResponse) => {
+          const responseBody = JSON.parse(result.body) as LoginResponse
+
+          expect(result.statusCode).toBe(200)
+          expect(responseBody.email).toBe(email)
+          expect(responseBody.name).toBe(name)
+          expect(responseBody.token.length).toBeGreaterThan(0)
+          expect(responseBody.id).toBeDefined()
+
+          let decoded: JwtContent = jwt.verify(responseBody.token, process.env.JWT_SECRET as string) as JwtContent
+
+          expect(decoded.id).toBeDefined()
+        })
+
+      // -- verify that user was added --
+
+      const { data: addedUserData } = await dbClient.query<FaunaQuery<UserModel>>(
+        Get(
+          Match(
+            Index("user_by_email"),
+            email
+          )
+        )
+      )
+
+      expect(addedUserData.name).toBe(name)
+      expect(addedUserData.email).toBe(email)
+    })
+  })
+})
